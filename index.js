b03b7136c03bce08a696a8649ba7880d97f80fed
Steen G. Sargent <steen.sargent@gmail.com>
2019-04-30 11:17:38 -0600

    add some middleware

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const debug = require('debug')('app:startup');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+
+if(app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    debug('Morgan enabled...')
+}
 
 const genres = [
     { id: 1, name: 'Action' },
